d2fc62e627059575cbd63fea852005215880aa3f
fakhrullhafiz <fakhrulhafiz99@gmail.com>
2023-04-02 00:40:31 +0800

    second commit

--- app/src/main/java/com/myapplication/MainActivity.java
+++ app/src/main/java/com/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.myapplication;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.View;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -26,5 +28,28 @@ public class MainActivity extends AppCompatActivity {
     public void privacyPolicy(View view) {
         Intent i = new Intent(getApplicationContext(), PrivacyPolicy.class);
         startActivity(i);
+    }
+
+    public void logOut(View view) {
+        logoutMenu(MainActivity.this);
+    }
+
+    private void logoutMenu(MainActivity mainActivity){
+        AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
+        builder.setTitle("Logout");
+        builder.setMessage("Are you sure you want to logout ?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
     }
 }
